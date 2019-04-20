9336a93f659ee75b7caca99adba0569d3cb625b9
Arjit kiledar <kiledar.a@northeastern.edu>
2019-04-20 12:51:37 -0400

    final

---
+++ AuthApi/api/config/passport.js
@@ -0,0 +1,28 @@
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+
+passport.use(new LocalStrategy({
+    usernameField : 'email'
+},
+function(username, password, done){
+    User.findOne({ email: username}, function(err,user) {
+        if(err) {
+            return done(err);
+        }
+        if(!user)
+        {
+            return done(null, false, {
+                message : 'user not found'
+            });
+        }
+        if(!user.validPassword(password)){
+            return(null, false, {
+                message : 'password is wrong'
+            });
+        }
+        return done(null, user);
+    })
+}
+));
